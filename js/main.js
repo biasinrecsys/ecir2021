2028c32228fdde7a7f2f34358fd80a1b3c851400
mirkomarras <m.marras19@gmail.com>
2020-10-10 02:13:38 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ jQuery(document).ready(function( $ ) {
     if ($(this).scrollTop() > 100) {
       $('.back-to-top').fadeIn('slow');
     } else {
+		
       $('.back-to-top').fadeOut('slow');
     }
   });
